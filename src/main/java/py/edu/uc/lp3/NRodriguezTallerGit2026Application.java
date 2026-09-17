365dcbc2c95eb5851deb1345fdca4550cc4b7c32
NicoRodriguez85677 <nicolasrodriguez85677@gmail.com>
2026-09-17 19:30:01 -0300

    chore: Spring Boot API REST desde start.spring.io

---
+++ src/main/java/py/edu/uc/lp3/NRodriguezTallerGit2026Application.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NRodriguezTallerGit2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NRodriguezTallerGit2026Application.class, args);
+	}
+
+}
